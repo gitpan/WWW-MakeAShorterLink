3a0d8889ee9f765bf0247b8db4ed07df13a32f60
Dave Cross <dave@perlhacks.com>
2002-08-28 19:54:19 +0000

    Import of DAVECROSS/WWW-MakeAShorterLink-1.03 from CPAN.
    gitpan-cpan-distribution: WWW-MakeAShorterLink
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         DAVECROSS/WWW-MakeAShorterLink-1.03.tar.gz
    gitpan-cpan-author:       DAVECROSS
    gitpan-cpan-maturity:     released

---
+++ test.pl
@@ -0,0 +1,25 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+#########################
+
+# change 'tests => 1' to 'tests => last_test_to_print';
+
+use Test;
+BEGIN { plan tests => 4 };
+use WWW::MakeAShorterLink;
+ok(1); # If we made it this far, we're ok.
+
+#########################
+
+# Insert your test code below, the Test module is use()ed here so read
+# its man page ( perldoc Test ) for help writing this test script.
+
+ok (makeashorterlink('http://dave.org.uk/scripts/webged-1.02.tar.gz')
+    eq 'http://makeashorterlink.com/?M328231A1');
+
+ok (makealongerlink('http://makeashorterlink.com/?M328231A1')
+    eq 'http://dave.org.uk/scripts/webged-1.02.tar.gz');
+
+ok (makealongerlink('M328231A1')
+    eq 'http://dave.org.uk/scripts/webged-1.02.tar.gz');
